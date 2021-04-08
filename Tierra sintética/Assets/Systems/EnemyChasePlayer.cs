6c57033b60033ae273b24603db2e4b2ee04f26bb
RobertoCH44 <rober_aletur@hotmail.com>
2021-04-08 11:51:42 -0500

    Sistema de enemigo
    El enemigo persigue al jugador cuando lo ve

---
+++ Tierra sintética/Assets/Systems/EnemyChasePlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SensorToolkit;
+
+public class EnemyChasePlayer : MonoBehaviour
+{
+    public Sensor sensor;
+    //public Animator animator;
+
+    void Update()
+    {
+        var detected = sensor.GetNearest();
+        if(detected != null)
+        {
+            Chase(detected);
+        }else
+        {
+            Idel();
+        }
+    }
+
+    void Chase(GameObject target)
+    {
+        var speed = 3f;
+
+        transform.LookAt(target.transform);
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        //animator.SetFloat("Speed_f", speed);
+
+        if((transform.position - target.transform.position).magnitude < 0.5f)
+        {
+            //Destroy(target);
+            Debug.Log("Hecho!");
+        }
+    }
+
+    void Idel()
+    {
+        //animator.SetFloat("Speed_f", 0);
+    }
+}
